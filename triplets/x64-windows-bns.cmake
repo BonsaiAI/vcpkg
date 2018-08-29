0959227873982d36bccacc369ded0feca85c4a1b
Oren Leiman <oren.leiman@bons.ai>
2018-08-28 23:02:06 -0700

    libbonsai (Version 2.0.4)
    Signed-off-by: Oren Leiman <oren.leiman@bons.ai>

--- triplets/x64-windows-bns.cmake
+++ triplets/x64-windows-bns.cmake
@@ -1,8 +1,12 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
-set(VCPKG_BUILD_TYPE release)
 
 if (PORT MATCHES "bonsai")
  set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
+
+if (PORT MATCHES "bonsai-simulink")
+ set(VCPKG_BUILD_TYPE release)
+endif()
+
